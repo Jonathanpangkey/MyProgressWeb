fda477d3f51e990ea4b3d40f3e284725442ff604
jonathanpangkey <jonathanpangkey912@gmail.com>
2023-03-06 09:53:48 +0800

    set up new database

--- app.js
+++ app.js
@@ -3,33 +3,36 @@ const articleRouter = require("./routes/article");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const Articles = require("./models/articles");
+require('dotenv').config();
 
 app = express();
 
-mongoose.connect(
-	"mongodb://<username>:<password>@cluster0-shard-00-00.6hfpn.mongodb.net:27017,cluster0-shard-00-01.6hfpn.mongodb.net:27017,cluster0-shard-00-02.6hfpn.mongodb.net:27017/?ssl=true&replicaSet=atlas-qqmuiu-shard-0&authSource=admin&retryWrites=true&w=majority",
-	{ useNewUrlParser: true },
-	{ useUnifiedTopology: true }
-);
-
-let db = mongoose.connection;
-
-db.once('open',function(){
-  console.log('Connected');
-});
+async function connectToDB() {
+	try {
+	  await mongoose.connect(process.env.MONGODB_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	  });
+	  console.log('Connected to MongoDB');
+	} catch (err) {
+	  console.error(err);
+	}
+  }
+  
+connectToDB();
+  
 
 app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 app.use(methodOverride("_method"));
 
 app.get("/", async (req, res) => {
-	const articles = await Articles.find().sort({ createAt: "desc" });
-	res.render("articles/index", {
-		articles: articles,
-	});
+  const articles = await Articles.find().sort({ createAt: "desc" });
+  res.render("articles/index", {
+    articles: articles,
+  });
 });
-
 
 app.use("/articles", articleRouter);
 
-app.listen( process.env.PORT || 80, () => console.log('app is running '));
+app.listen(process.env.PORT || 80, () => console.log("app is running "));
